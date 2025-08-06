b802a0bc6a104cb196bbff15d6ffdda9ef8d3ced
ashtonx24 <asharc789@gmail.com>
2025-08-06 15:25:59 +0530

    Minor update

--- pdf_metadata_writer.py
+++ pdf_metadata_writer.py
@@ -1,8 +1,8 @@
 from PyPDF2 import PdfReader, PdfWriter
 
-input_pdf = "path/to/your/original_pdf.pdf"  # Full path to your PDF
-output_pdf = "path/to/your/updated_pdf.pdf"    # Where you want to save the updated file
-
+input_pdf = r"path/to/your/original_pdf.pdf"  # Full path to your PDF
+output_pdf = r"path/to/your/updated_pdf.pdf"    # Where you want to save the updated file
+# add or remove that r. it's just for your safety if you get an error in the encoding.
 reader = PdfReader(input_pdf)
 writer = PdfWriter()
 
